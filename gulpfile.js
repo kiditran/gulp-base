a2bba517ab69f87f091a322bb53703f0b4783585
Hieu Kidi <hieutm@mintpotvn.com>
2021-11-02 11:24:48 +0700

    Create src instead folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,29 +10,27 @@ var cleanCss = require("gulp-clean-css");
 // Compile CSS
 function buildStyles() {
   return gulp
-    .src("./sass/**/*.scss")
+    .src("./src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(concat("styles.css")) // combine css files
     .pipe(cleanCss()) // minify css file
     .pipe(gulp.dest("./dist/css"));
 }
-
 exports.buildStyles = buildStyles;
 
 // Combine JS files
 function combineJS() {
   return gulp
-    .src("./js/*.js")
+    .src("./src/js/*.js")
     .pipe(concat("all.js")) // combine js files
     .pipe(uglify()) // minify js file
     .pipe(gulp.dest("./dist/js"));
 }
-
 exports.combineJS = combineJS;
 
 exports.default = function () {
   // You can use a single task
-  watch("./sass/**/*.scss", series(buildStyles));
+  watch("./src/sass/**/*.scss", series(buildStyles));
   // Or a composed task
-  watch("./js/*.js", series(combineJS));
+  watch("./src/js/*.js", series(combineJS));
 };
