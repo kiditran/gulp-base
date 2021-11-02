b8c27d3c02d3fc3343923adb9e98896e2477bb6b
Hieu Kidi <hieutm@mintpotvn.com>
2021-11-02 10:42:13 +0700

    Add minify CSS, JS files

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,35 @@ const { series, parallel, watch } = require("gulp");
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var cleanCss = require("gulp-clean-css");
 
 // Compile CSS
 function buildStyles() {
   return gulp
     .src("./sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./css"));
+    .pipe(concat("styles.css")) // combine css files
+    .pipe(cleanCss()) // minify css file
+    .pipe(gulp.dest("./dist/css"));
 }
-// Combine JS files
-gulp.task("scripts", function () {
-  return gulp
-    .src("./js/*.js")
-    .pipe(concat("all.js"))
-    .pipe(gulp.dest("./dist/"));
-});
 
 exports.buildStyles = buildStyles;
+
+// Combine JS files
+function combineJS() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(concat("all.js")) // combine js files
+    .pipe(uglify()) // minify js file
+    .pipe(gulp.dest("./dist/js"));
+}
+
+exports.combineJS = combineJS;
 
 exports.default = function () {
   // You can use a single task
   watch("./sass/**/*.scss", series(buildStyles));
   // Or a composed task
-  watch("src/lib/*.js", series(clean, javascript));
+  watch("./js/*.js", series(combineJS));
 };
